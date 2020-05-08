0bdd6cfc79de5c8855c24f61a651f070b4416ff5
Livia <liviapereira1997@hotmail.com>
2020-05-08 17:13:53 -0300

    adding login page

---
+++ LOGIN.php
@@ -0,0 +1,80 @@
+<?php
+
+// Includes
+include("./createProduto.php");
+
+// Definindo o valor para login
+$loginOk = true;
+
+// Verificar se o form de login foi enviado
+if($_GET){
+
+    // Buscar um usuário com o email enviado no $_GET['email']
+    $email = $_GET['email'];
+    $senha = $_GET['senha'];
+    $usuario = buscarUsuarios();
+
+    foreach($usuario as $usuario){
+        
+        // testando se usuário com o email passado
+        if($usuario['email'] == $email){
+            
+            // Usuário está ok. Testar senha
+            //if($usuario['senha'] == $senha){
+                if(password_verify($senha,$usuario['senha'])){
+
+                //iniciar a session
+                session_start();
+
+                $_SESSION['email'] = $usuario['email'];
+        
+                // Redirecionando para a página que lista usuarios
+                header('location: ./createProduto.php');
+
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>LOGIN</h1>
+    
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">Suas informações estão seguras.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Lembrar de mim</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>  
+</body>
+</html>
