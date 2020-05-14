050762f73936e97ee6bc6fd25661489fdd09d3d0
Livia <liviapereira1997@hotmail.com>
2020-05-13 21:05:05 -0300

    adding the page indexProdutos.php

---
+++ indexProdutos.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/indexProdutos.css">
+    <title>Tabela Produtos</title>
+</head>
+<body>
+
+<h1>TODOS OS PRODUTOS</h1>
+    
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li id="titulo" class="list-group-item">Nome</li>
+  <li id="titulo" class="list-group-item">Categoria</li>
+  <li id="titulo" class="list-group-item">Preço</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Camiseta</li>
+  <li class="list-group-item">Vestuário</li>
+  <li class="list-group-item">R$49,99</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Caneca</li>
+  <li class="list-group-item">Utensílios</li>
+  <li class="list-group-item">R$25,00</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Televisão</li>
+  <li class="list-group-item">Eletrodomésticos</li>
+  <li class="list-group-item">R$500,00</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Mouse Gamer</li>
+  <li class="list-group-item">Acessórios</li>
+  <li class="list-group-item">R$30,00</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Alcool Gel</li>
+  <li class="list-group-item">Higiene</li>
+  <li class="list-group-item">R$25,00</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Adega Climatizada</li>
+  <li class="list-group-item">Eletrodomésticos</li>
+  <li class="list-group-item">R$450,00</li>
+</ul>
+<ul id="tabela" class="list-group list-group-horizontal">
+  <li class="list-group-item">Notebook ASUS</li>
+  <li class="list-group-item">Tecnologia</li>
+  <li class="list-group-item">R$900,00</li>
+</ul>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>  
+</body>
+</html>
