44db2908218a4fee7b217e0b87a372e6f4b8b797
Livia <liviapereira1997@hotmail.com>
2020-05-13 21:06:11 -0300

    adding the file showProduto

---
+++ showProduto.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/showProduto.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="media">
+  <img src="./img/caneca.jpg" class="align-self-center mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-0">Caneca Game of Thrones</h5>
+    <p><h6><b>Categoria:</h6></b> Utensílios</p>
+    <p class="mb-0"><h6><b>Descrição:</h6></b> Caneca de chopp, de aço inoxidável, personalizada Game of Thrones.</p>
+    <p><h6><b>Quantidade em estoque:</h6></b> 55</p>
+  </div>
+</div>
+
+
+<button type="button" class="btn btn-lg btn-primary" disabled><--Voltar para a lista de produtos </button>
+<button type="button" class="btn btn-secondary btn-lg" disabled>Sair</button>
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>  
+</body>
+</html>
